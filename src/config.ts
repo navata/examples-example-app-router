0d6029009f2ee913008690e7bec303a8ca364a39
thainv92 <thainv@pharmacity.vn>
2024-03-03 16:31:16 +0700

    Init

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+import {Pathnames} from 'next-intl/navigation';
+
+export const locales = ['en', 'de'] as const;
+
+export const pathnames = {
+  '/': '/',
+  '/pathnames': {
+    en: '/pathnames',
+    de: '/pfadnamen'
+  }
+} satisfies Pathnames<typeof locales>;
+
+// Use the default: `always`
+export const localePrefix = 'never';
+
+export type AppPathnames = keyof typeof pathnames;
+
+export const localeDetection = false;
